1d4a22e319c87d55eb1ddad51d8ba9bc1776b35b
Mohamed Sharfan <sharfansaleem72@gmail.com>
2025-09-20 20:10:09 +0530

    routes done

--- backend/main.py
+++ backend/main.py
@@ -23,3 +23,38 @@ app.add_middleware(
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME")
 
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+
+
+class Annotation(BaseModel):
+    user: str
+    date: str
+    layer: str
+    type: str
+    coordinates: list
+
+@app.post("/annotations")
+def create_annotation(annotation: Annotation):
+    result = annotations_collection.insert_one(annotation.dict())
+    return {"id": str(result.inserted_id)}
+
+
+@app.get("/annotations")
+def get_annotations(date: str = None, layer: str =None):
+    query = {}
+    if date:
+        query["date"] = date
+    if layer:
+        query["layer"] = layer
+    results = list(annotation_collection.find(query))
+    for r in results:
+        r["_id"] = str(r["_id"])
+    return results
+
+@app.delete("/annotations/{annotation_id}")
+def delete_annotation(annotation_id: str):
+    result = annotations_collection.delete_one({"_id" : ObjectId(annotation_id)})
+    if result.deleted_count == 1:
+        return { "message": "Deleted" }
+    return { "message": "Not found" }
